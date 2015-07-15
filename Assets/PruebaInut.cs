b557056a3f317e1fa1e179afa73ea69a3b4d18a5
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-07-15 13:07:52 +0100

    First version of menu and game gui

---
+++ Assets/PruebaInut.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PruebaInut : MonoBehaviour {
+    public LevelManager levelManager;
+    public ScoreController scoreController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.A))
+            levelManager.playerDied();
+        if (Input.GetKeyDown(KeyCode.S))
+            scoreController.addScore(1);
+	}
+}
