cf54db912d1ed29f22fedc0c6b0096dff30875e4
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-07-15 11:53:02 +0100

    First commit

---
+++ Assets/CharacterManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterManager : MonoBehaviour
+{
+
+
+
+	private Rigidbody2D myRigidbody;
+	private Vector3 AimPoint;
+	public int speed = 10;
+	public int bulletSpeed = 100;
+	public GameObject bullet;
+	public GameObject canon;
+
+	// Use this for initialization
+	void Start ()
+	{
+		myRigidbody = this.GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Movement ();
+		Aim ();
+		Shoot ();
+
+	}
+
+	void Movement ()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		myRigidbody.velocity = Vector2.up * v * speed + Vector2.right * h * speed;
+	}
+
+	void Aim ()
+	{
+		AimPoint = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 10f));
+		this.transform.right = AimPoint - this.transform.position;
+
+	}
+
+	void Shoot ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+
+			GameObject bulletInstance = Instantiate (bullet, canon.transform.position, this.transform.rotation) as GameObject;
+
+			bulletInstance.GetComponent<Rigidbody2D> ().velocity = bulletSpeed * (canon.transform.position - this.transform.position);
+			Destroy(bulletInstance,2.0f);
+		
+		}
+	}
+
+}
---
+++ Assets/enemyController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyController : MonoBehaviour {
+
+
+	private GameObject Player;
+	private Rigidbody2D myRigidbody;
+
+	public int speed;
+	// Use this for initialization
+	void Start () {
+		Player=GameObject.Find ("MainCharacter");
+		myRigidbody = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 direction = Player.transform.position - this.transform.position;
+
+		myRigidbody.velocity = direction * speed;
+		Vector3 moveTowards = (Player.transform.position - this.transform.position).normalized;
+		moveTowards.z = 0f;
+		this.transform.right = moveTowards;
+
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Bullet") {
+			Destroy (other.gameObject);
+			Destroy (this.gameObject);
+		} else if (other.gameObject.tag == "Player") {
+			Debug.Log("endGame");
+		
+		}
+	}
+}
